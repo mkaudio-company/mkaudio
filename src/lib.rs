3dd6e959d29df11acef6fb71039005ce12298ded
Minjae Kim <kminjae00@naver.com>
2024-01-22 11:38:00 +0900

    io

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 pub struct AudioBuffer
 {
     channels : u32,
@@ -130,50 +132,108 @@ impl Drop for AudioBuffer
     }
 }
 
-pub struct AudioDevice
+pub struct AudioDevice<R : std::io::Read, W : std::io::Write>
 {
     name : String,
     sample_rate : u32,
-    buffer : AudioBuffer,
-    state : bool
+    pub buffer : AudioBuffer,
+    state : bool,
+    reader : std::io::BufReader<R>,
+    writer : std::io::BufWriter<W>
 }
-impl AudioDevice
+impl<R : std::io::Read, W : std::io::Write> AudioDevice<R, W>
 {
+    pub fn init(name : &str, reader : R, writer : W) -> Self
+    {
+        Self
+        {
+            name: String::from(name),
+            sample_rate: u32::default(),
+            buffer: AudioBuffer::default(),
+            state: bool::default(),
+            reader: std::io::BufReader::new(reader),
+            writer: std::io::BufWriter::new(writer),
+        }
+    }
     pub fn name(&self) -> String { self.name.clone() }
-    pub fn sample_rate(&self) -> u32 { self.sample_rate }
-    pub fn buffer_size(&self) -> u32 { self.buffer.buffer_size }
-    pub fn get_channel(&self) -> u32 { self.buffer.channels }
-    pub fn get_depth(&self) -> u32 { self.buffer.bit_depth }
+    pub fn get_sample_rate(&self) -> u32 { self.sample_rate }
+    pub fn set_sample_rate(&mut self, sample_rate : u32)
+    {
+        if sample_rate % 22050 == 0
+        {
+            self.sample_rate = sample_rate;
+            let state_data = ((sample_rate / 22050) as u8 - 1) << 2 | 0b01;
+            self.inner_write(0, state_data);
+            return;
+        }
+        else if sample_rate % 24000 == 0
+        {
+            self.sample_rate = sample_rate;
+            let state_data = ((sample_rate / 24000) as u8 + 31) << 2 | 0b01;
+            self.inner_write(0, state_data);
+            return;
+        }
+        println!("Invalid sample rate!");
+    }
+    pub fn get_buffer_size(&self) -> u32 { self.buffer.buffer_size }
+    pub fn set_buffer_size(&mut self, buffer_size : u32)
+    {
+        if buffer_size % 32 == 0
+        {
+            self.buffer.buffer_size = buffer_size;
+            let state_data = ((buffer_size / 32) as u8) << 2 | 0b10;
+            self.inner_write(0, state_data);
+            return;
+        }
+        println!("Invalid buffer size!");
+    }
+    pub fn channels(&self) -> u32 { self.buffer.channels }
+    pub fn bit_depth(&self) -> u32 { self.buffer.bit_depth }
     pub fn read(&mut self)
     {
         self.state = true;
         while self.state
         {
-
+            let buffer = self.reader.buffer();
+            let len = buffer.len();
+            if len == 0 
+            {
+                println!("Buffer is empty!");
+                continue;
+            }
+            let state_var = buffer[0] & 0b11;
+            let state_data = buffer[0] >> 2;
+            if state_var == 0b01
+            {
+                self.sample_rate = if state_data < 32 { (state_data + 1) as u32 * 22050 } else { (state_data - 31) as u32 * 24000  }
+            }
+            else if state_var == 0b10 { self.buffer.buffer_size = (state_data + 1) as u32 * 32 }
+            else if state_var == 0b11 { self.buffer.channels = (state_data as u32) + 1 }
+            if len == 1 { continue; }
+            for index in 1..len { unsafe { *self.buffer.data.offset(index as isize) = buffer[index]; } }
+            std::thread::sleep(std::time::Duration::from_secs_f64(1.0 / self.sample_rate as f64));
         }
     }
     pub fn write(&mut self)
     {
         self.state = true;
+        let mut count = 0;
         while self.state
         {
-
+            self.inner_write(count, 0);
+            std::thread::sleep(std::time::Duration::from_secs_f64(1.0 / self.sample_rate as f64));
+            count += 1;
+        }
+    }
+    fn inner_write(&mut self, count : usize, state_var : u8)
+    {
+        if count == 0 { let _ = self.writer.write(&[state_var]); }
+        else
+        {
+            let mut data = vec![0; self.buffer.buffer_size as usize + 1];
+            for index in 1..data.len() { data[index] = unsafe { *self.buffer.data.offset(index as isize) }; }
+            let _ = self.writer.write(&data);
         }
     }
     pub fn stop(&mut self) { self.state = false; }
 }
-impl std::default::Default for AudioDevice
-{
-    fn default() -> Self 
-    {
-        Self
-        {
-            name: Default::default(),
-            sample_rate: Default::default(),
-            buffer: Default::default(),
-            state: Default::default()
-        }
-    }
-}
-unsafe impl Sync for AudioDevice { }
-unsafe impl Send for AudioDevice { }
